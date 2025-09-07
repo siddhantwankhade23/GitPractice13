97f2af686704e837911a52d202b37cd14a4eba14
Siddhant Wankhade <siddhantwankhade23@gmail.com>
2025-09-07 21:40:37 +0530

    master commit 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,6 +15,8 @@ public class Main {
             System.out.println("1");
             System.out.println("2");
             System.out.println("3");
+
+            System.out.println("Master 1");
         }
     }
 }
